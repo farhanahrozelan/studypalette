51258eaa2a80e5f30560cb618c673dd5e17f5f9f
farhanahrozelan <118259234+farhanahrozelan@users.noreply.github.com>
2024-12-23 01:20:30 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/views/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+   
+            colors:{
+                customColor:{
+                primary: '#DBA39A',
+                secondary: '#F2E8E8',
+            },
+
+        },
+    },
+
+    plugins: [forms, typography],
+}
+
+};
